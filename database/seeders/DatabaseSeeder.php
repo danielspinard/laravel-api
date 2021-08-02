8f46e68923bda5cda24a14278ec421c2db7b54af
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-08-02 14:55:45 -0300

    Run factory in DatabaseSeeder CovidCase

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\CovidCase::factory(10)->create();
     }
 }
