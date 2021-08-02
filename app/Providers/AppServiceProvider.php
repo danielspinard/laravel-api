65b2d7a97c6dcf5c6ee2c5b2977a6dd8502d3d62
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-08-02 15:40:15 -0300

    Fix CovidCase observe CovidCaseObserver in boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\CovidCase;
+use App\Observers\CovidCaseObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+        CovidCase::observe(CovidCaseObserver::class);
     }
 }
