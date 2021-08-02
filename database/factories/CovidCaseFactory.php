c10421d3a65cbb65f2d2c04d45a1e47041782635
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-08-02 14:53:57 -0300

    Make CovidCaseFactory

---
+++ database/factories/CovidCaseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CovidCase;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CovidCaseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = CovidCase::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
