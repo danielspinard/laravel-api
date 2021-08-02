7ad201d41d72f4c8be1dc3b6cf956c2081f06e58
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-08-02 15:45:03 -0300

    Fix delete CovidCase cache when new CovidCase is created

--- app/Observers/CovidCaseObserver.php
+++ app/Observers/CovidCaseObserver.php
@@ -2,62 +2,17 @@
 
 namespace App\Observers;
 
+use Illuminate\Support\Facades\Cache;
 use App\Models\CovidCase;
 
 class CovidCaseObserver
 {
     /**
-     * Handle the CovidCase "created" event.
-     *
-     * @param  \App\Models\CovidCase  $covidCase
+     * @param CovidCase $covidCase
      * @return void
      */
-    public function created(CovidCase $covidCase)
+    public function created(CovidCase $covidCase): void
     {
-        //
-    }
-
-    /**
-     * Handle the CovidCase "updated" event.
-     *
-     * @param  \App\Models\CovidCase  $covidCase
-     * @return void
-     */
-    public function updated(CovidCase $covidCase)
-    {
-        //
-    }
-
-    /**
-     * Handle the CovidCase "deleted" event.
-     *
-     * @param  \App\Models\CovidCase  $covidCase
-     * @return void
-     */
-    public function deleted(CovidCase $covidCase)
-    {
-        //
-    }
-
-    /**
-     * Handle the CovidCase "restored" event.
-     *
-     * @param  \App\Models\CovidCase  $covidCase
-     * @return void
-     */
-    public function restored(CovidCase $covidCase)
-    {
-        //
-    }
-
-    /**
-     * Handle the CovidCase "force deleted" event.
-     *
-     * @param  \App\Models\CovidCase  $covidCase
-     * @return void
-     */
-    public function forceDeleted(CovidCase $covidCase)
-    {
-        //
+        Cache::forget('cases');
     }
 }
