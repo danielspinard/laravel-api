18fe28d8495c5da1ad9c702b60d3e9ef793116c6
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-08-02 15:04:05 -0300

    Add method to return EloquentCollection of CovidCase

--- app/Http/Controllers/Api/CaseController.php
+++ app/Http/Controllers/Api/CaseController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use App\Models\CovidCase;
 
 class CaseController extends Controller
 {
+    /**
+     * @return EloquentCollection
+     */
+    public function index(): EloquentCollection
+    {
+        return CovidCase::all();
+    }
 }
