144ed03044b8d96a4ff403c68735b638aed06614
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-08-02 14:56:55 -0300

    Add return type in methods on CreateCovidCasesTable

--- database/migrations/2021_08_02_173445_create_covid_cases_table.php
+++ database/migrations/2021_08_02_173445_create_covid_cases_table.php
@@ -7,11 +7,9 @@ use Illuminate\Support\Facades\Schema;
 class CreateCovidCasesTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('covid_cases', function (Blueprint $table) {
             $table->id();
@@ -22,11 +20,9 @@ class CreateCovidCasesTable extends Migration
     }
 
     /**
-     * Reverse the migrations.
-     *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('covid_cases');
     }
